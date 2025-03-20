e43de728388f5f5e353529d8b90ae5c6e4d76854
AmanpreetSingh0071 <amanpreet.m.ahluwalia@gmail.com>
2025-03-20 23:27:02 +0000

    Fixing Python 3.10 issues in Streamlit deployment

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Use Python 3.10 directly
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
-
-# Run Streamlit
-python3.10 -m streamlit run main.py
